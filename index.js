874a9c5b3408ab83c7c785fdf8462d71ef626a8b
Joshua Johnsgaard <hello@gaard.ca>
2019-08-27 23:22:30 -0600

    Adds conditions everywhere for poop and flush commands

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ const app = express();
 // The port used for Express server
 const PORT = 3000;
 
+let bathrooms = {
+  gents: {
+    stalls: 1,
+    shower: 2,
+  },
+  ladies: {
+    stalls: 3,
+    shower: 1,
+  },
+};
+
 // Starts server
 app.listen(process.env.PORT || PORT, function() {
   console.log('Bot is listening on port ' + PORT);
@@ -18,10 +29,31 @@ app.listen(process.env.PORT || PORT, function() {
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.post('/', (req, res) => {
+  let responseMessage = ':poop: Something went wrong! :poop:';
+  if (bathrooms[req.body.text]) {
+    if (req.body.command === '/poop') {
+      bathrooms[req.body.text].stalls = bathrooms[req.body.text].stalls - 1;
+    } else if (req.body.command === '/flush') {
+      bathrooms[req.body.text].stalls = bathrooms[req.body.text].stalls + 1;
+      if (req.body.text === 'ladies' && bathrooms[req.body.text].stalls >= 3) {
+        bathrooms[req.body.text].stalls = 3;
+      } else if (req.body.text === 'gents' && bathrooms[req.body.text].stalls >= 1) {
+        bathrooms[req.body.text].stalls = 1;
+      }
+    }
+    if (bathrooms[req.body.text].stalls < 0) {
+      bathrooms[req.body.text].stalls = 0;
+      responseMessage = 'There are no more stalls left for the ' + req.body.text;
+    } else {
+      responseMessage = 'The ' + req.body.text + ' have ' + bathrooms[req.body.text].stalls + ' stall left ';
+    }
+  } else {
+    responseMessage = 'The bathrooms do not have a ' + '`' + req.body.text+'`' + ' type :confused:.\n This command accepts [ ladies, gents ].';
+  }
 
   var data = {
     response_type: 'in_channel', // public to the channel 
-    text: 'Hi :wave: \n This is Joshua\'s bot!', 
+    text: responseMessage, 
   };
 
   request.post('https://slack.com/api/chat.postMessage', data, function (error, response, body) {
